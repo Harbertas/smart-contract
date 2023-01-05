5e907588d365a4bb3a4c8562325c29269647e140
Hubertas Klangauskas <93277255+Harbertas@users.noreply.github.com>
2023-01-05 02:11:48 +0200

    update

--- smart-contract/contracts/Deal.sol
+++ smart-contract/contracts/Deal.sol
@@ -1,72 +1,125 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
 contract Deal {
-	struct Product
-	{
-		string name;
-		uint quantity;
-		uint price; // Price per single product.
-	}
 
-	struct Shipment
-	{
-		address payable courierAddr;
-		uint price;
-		uint date;
-	}
+    //STAGES
+    enum Stage {Init, SetUp, Bet, Play}
 
-	struct Order
-	{
-		Product product;
-		Shipment shipment;
-		uint total;
-		bool initialized;
-	}
+    Stage public stage = Stage.Init;
 
-	address public buyerAddr; // payable - Enable sending money to them.
-	address payable public sellerAddr; // payable - Enable sending money to them.
+    //EVENTS
+    event bidded(uint id, address bidder, uint price);
 
-	mapping (uint => Order) public orders;
-	uint public orderCount;
+    //Storage variables
+    mapping(address => uint256) registeredFunds;
+    mapping(address => uint8) chosenNumber;
+    mapping(address => bool) approvedPlayer;
+    mapping(address => uint256) bets;
+    address[] players;
+    uint256 c;
+    uint8[] endings = [2, 3, 4, 5, 6, 7, 8 , 9, 10, 11, 12];
 
-	event OnOrderPlaced(address buyerAddr, string product, uint quantity, uint orderNumber);
+    //MODIFIERS
+    modifier validStage(Stage reqStage)
+    { 
+        require(stage == reqStage);
+        _;
+    }
 
-	constructor(address buyer) {
-		sellerAddr = payable(msg.sender);
-		buyerAddr = buyer;
-		orderCount = 0;
-	}
+    modifier isExecutor()
+    { 
+        require(msg.sender == executor, "Only The Executor Can Change the Bet");
+        _;
+    }
 
-	function placeOrder(string calldata product, uint quantity, address courierAddr) public payable {
-		require(sellerAddr == msg.sender);
+    uint startTime;
+    address payable public executor;
 
-		uint total = 10 * quantity + 5;
+    constructor() {
+        executor = payable(msg.sender);
+        stage = Stage.SetUp;
+        startTime = block.timestamp;
+    }
 
-		orders[orderCount] = Order(
-			Product(product, quantity, 10),
-			Shipment(payable(courierAddr), 5, block.timestamp),
-			total,
-			true
-		);
+    function start() public validStage(Stage.SetUp){
+        if (approvedPlayer[msg.sender] != true) {
+            approvedPlayer[msg.sender] = true;
+            players.push(msg.sender);
+        }
+        if(players.length > 1) {
+            //stage = Stage.Bet;
+        }
+    }
 
-		uint id = orderCount;
-		orderCount++;
+    function increaseFunds() public payable {
+        require(msg.value > 0, "Must send ETH");
+        registeredFunds[msg.sender] += msg.value;
+    }
 
-		emit OnOrderPlaced(
-			buyerAddr,
-			product,
-			quantity,
-			id
-		);
-	}
+    function bet(uint8 number, uint256 amount) public validStage(Stage.Bet){
+        require(approvedPlayer[msg.sender], "You Are Not Approved");
+        require(registeredFunds[msg.sender] >= amount, "Not enough Funds");
+        require(number > 1 && number < 13, "Choose Between [2-12]");
+        require(bets[msg.sender] == 0, "You Have Already Betted");
+        bets[msg.sender] = amount;
+        chosenNumber[msg.sender] = number;
+        registeredFunds[msg.sender] -= amount;
+        c++;
+        if(c == players.length) stage = Stage.Play;
+    }
 
-	function getOrder(uint number) view public returns(address buyer, string memory product, uint quantity, uint total, uint date) {
-		require(orders[number].initialized);
+    function spin() public validStage(Stage.Play) returns(uint256){
+        require(msg.sender == executor, "Only Executor Can Start The Game");
+        address[] memory newArray;
+        uint256 number = 5;
+        for(uint256 i = 0; i < players.length; i++) {
+            address playerAddress = players[i];
+            if(bets[playerAddress] > 0) {
+                 if(chosenNumber[playerAddress] == number) {
+                    bets[playerAddress]*=2;
+                    registeredFunds[playerAddress] += bets[playerAddress];
+                 }
+                chosenNumber[playerAddress] = 0;
+                bets[playerAddress] = 0;
+            }
+            approvedPlayer[playerAddress] = false;
+        }
+        players = newArray;
+        stage = Stage.SetUp;
+        c = 0;
+        return number;
+    }
 
-		return (
-			buyerAddr,
-			orders[number].product.name,
-			orders[number].product.quantity,
-			orders[number].product.price,
-			orders[number].shipment.date
-		);
-	}
+    function widthdraw() public {
+        require(address(this).balance >= registeredFunds[msg.sender], "Not enough funds in the bank");
+        require(registeredFunds[msg.sender] != 0, "You Have Nothing to Withdraw");
+        uint256 fund = registeredFunds[msg.sender];
+        //address(this).call{value: fund}(""); 
+
+        (bool wasSuccessful, ) = msg.sender.call{value: fund}("");
+        registeredFunds[msg.sender] -= fund;
+        require(wasSuccessful, "ETH transfer failed");
+    }
+
+    function getFunds() public view returns(uint256) {
+        return registeredFunds[msg.sender];
+    }
+
+    function bankFunds() public view returns(uint256) {
+        return address(this).balance;
+    }
+
+    function playersSize() public view returns(uint) {
+        return players.length;
+    }
+
+    function randomNumber() private view returns(uint256) {
+        uint256 number = 2 + uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % 11;
+        return number;
+    }
+    function changeStateBet() public isExecutor(){
+        stage = Stage.Bet;
+    }
 }
